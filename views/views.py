2c28f3b7e042c78db3239262652a1f6c5e1c9667
Karthikeyan ANBARASU <karthikeyansa39@gmail.com>
2021-07-20 21:26:01 +0530

    views

--- views/views.py
+++ views/views.py
@@ -1,9 +1,10 @@
-from flask import render_template, request, url_for, session, redirect, jsonify, flash
+from flask import render_template, request, url_for, session, redirect, jsonify
 from google.cloud.ndb._datastore_query import Cursor
 from models.models import Users, Tasks
 from main import app
 
 # login
+
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -11,31 +12,35 @@ def login():
                                             Users.password == request.json.get("password")).get()
         if current_user:
             session['current_user_id'] = current_user.key.id()
-            flash("Logged In Successful")
-            return render_template("add.html")
+            return jsonify({"success": True})
         else:
-            flash("Invalid credentials")
-            return redirect(url_for('login'))
+            return jsonify({"success": False})
     if session.get('current_user_id'):
-        username = Users.get_by_id(session.get('current_user_id')).email.split("@")[0]
-        return render_template("add.html", user_name = username)
+        return redirect(url_for('firstpage'))
     return render_template("login.html")
+
+
+@app.route("/tasks", methods=["GET"])
+def firstpage():
+    username = Users.get_by_id(session.get(
+        'current_user_id')).email.split("@")[0]
+    return render_template("add.html", user_name=username)
 
 
 # register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        new_user = Users(email=request.json.get("email"), password=request.json.get("password"))
+        new_user = Users(email=request.json.get("email"),
+                         password=request.json.get("password"))
         current_user = Users.query().filter(Users.email == request.json.get("email"),
                                             Users.password == request.json.get("password")).get()
-        if not current_user:
+        print(new_user.email)
+        if not current_user and "@" in new_user.email and ".com" in new_user.email:
             new_user.put()
-            flash("Registration Successful")
-            return redirect(url_for('login'))
+            return jsonify({"success": True})
         else:
-            flash("Registration Unsuccessful")
-            return redirect(url_for('register'))
+            return jsonify({"success": False})
     return render_template("register.html")
 
 
@@ -44,17 +49,20 @@ def register():
 def addTask():
     current_user = Users.get_by_id(session.get('current_user_id'))
     if request.method == "POST":
-        add_task = Tasks(content=request.json.get('content'), author=current_user.key)
+        add_task = Tasks(content=request.json.get(
+            'content'), author=current_user.key)
         add_task.put()
         new_task = {"id": add_task.key.id(), "content": add_task.content, "status": add_task.status,
                     "time": add_task.timestamp}
         return jsonify({"new_task": new_task, "success": True}), 200
 
+
 @app.route("/v1/todos/", methods=["GET"])
 def getTask():
     current_user = Users.get_by_id(session.get("current_user_id"))
     if request.method == "GET":
-        tasks = Tasks.query().filter(Tasks.author == current_user.key).order(-Tasks.timestamp).fetch()
+        tasks = Tasks.query().filter(
+            Tasks.author == current_user.key).order(-Tasks.timestamp).fetch()
         result = []
         complete = 0
         incomplete = 0
@@ -63,7 +71,8 @@ def getTask():
                 incomplete += 1
             elif(task.status == True):
                 complete += 1
-            result.append({"id": task.key.id(), "content": task.content, "status": task.status, "time": task.timestamp})
+            result.append({"id": task.key.id(), "content": task.content,
+                          "status": task.status, "time": task.timestamp})
         return jsonify({"result": result, "success": True, "incomplete": incomplete, "complete": complete})
 
 
@@ -89,7 +98,7 @@ def statusUpdate(id):
     if task:
         task.status = True
         task.put()
-        return jsonify({"task": {"id": task.key.id(), "content": task.content, "status": task.status, "time": task.timestamp},"success": True}), 200
+        return jsonify({"task": {"id": task.key.id(), "content": task.content, "status": task.status, "time": task.timestamp}, "success": True}), 200
     else:
         return jsonify({"success": False}), 500
 
