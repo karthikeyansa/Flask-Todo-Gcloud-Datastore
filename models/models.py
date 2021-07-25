769dffcb779e0ab558a439685e86cae1299d4ba2
Karthikeyan ANBARASU <karthikeyansa39@gmail.com>
2021-07-25 23:22:18 +0530

    models

---
+++ models/models.py
@@ -0,0 +1,18 @@
+from google.cloud import ndb
+
+
+class Users(ndb.Model):
+   email = ndb.StringProperty()
+   password = ndb.StringProperty()
+
+
+class Tasks(ndb.Model):
+   task_id = ndb.IntegerProperty(default=0)
+   description = ndb.TextProperty()
+   status = ndb.BooleanProperty(default=False)
+
+
+class Notes(ndb.Model):
+   content = ndb.TextProperty()
+   author = ndb.KeyProperty(Users)
+   taskOwner = ndb.StructuredProperty(Tasks, repeated=True, indexed=True)
